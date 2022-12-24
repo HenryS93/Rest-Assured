a6ed5221322c75206ff1c15dce62a642810157d6
Henry Sanchez <henrysanchez1993@hotmail.com>
2022-12-23 22:23:44 -0500

    Day06 -> start from 3.05 hr

---
+++ src/test/java/cydeo/day06/P01_HRDeserilization.java
@@ -0,0 +1,83 @@
+package cydeo.day06;
+
+import cydeo.Utilities.HrTestBase;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+import java.util.List;
+import java.util.Map;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+public class P01_HRDeserilization extends HrTestBase {
+
+    /**
+     *
+     * Create a test called getlocation
+     * 1.Send request to GET / locations
+     * 2.log uri to see
+     * 3.get all json as a map and print out screen all the things with the help of map
+     * System.out.println("============== GET FIRST LOCATION ============== " );
+     * System.out.println("============== GET FIRST LOCATION LINKS ============== " );
+     * System.out.println("============== GET ALL LOCATIONS AS A LIST OF MAPS ============== " );
+     * System.out.println("============== FIRST LOCATION ============== " );
+     * System.out.println("============== GET FIRST LOCATION ID ============== " );
+     * System.out.println("============== GET FIRST LOCATION FIRST LINK ============== " );
+     * System.out.println("============== LAST LOCATION ID ============== " );
+     */
+
+    @DisplayName("GET Locations From HR")
+    @Test
+    public void getLocation(){
+
+        JsonPath jsonPath = given().log().uri().when().get("/locations").then().statusCode(200).extract().response().jsonPath();
+
+
+        System.out.println("============== GET FIRST LOCATION ============== " );
+        Map<String, Object> pathMap = jsonPath.getMap("items[0]");
+        System.out.println("pathMap = " + pathMap);
+
+        System.out.println("============== GET FIRST LOCATION LINKS ============== " );
+        Map<String, Object> firstLink = jsonPath.getMap("items[0].links[0]");
+        System.out.println("firstLink = " + firstLink);
+        //To get href value
+        System.out.println("firstLink.get(\"href\") = " + firstLink.get("href"));
+
+
+        System.out.println("============== GET ALL LOCATIONS AS A LIST OF MAPS ============== " );
+        System.out.println("LIST:");
+        List<Map<String,Object>> allLocationMap = jsonPath.getList("items");
+        System.out.println("allLocationMap = " + allLocationMap);
+
+        for (Map<String,Object> eachmap : allLocationMap){
+            System.out.println("eachmap = " + eachmap);
+        }
+
+         System.out.println("============== FIRST LOCATION FROM ALL LOCATIONS ============== " );
+         System.out.println(allLocationMap.get(0));
+
+        System.out.println("============== GET FIRST LOCATION ID ============== " );
+        System.out.println(allLocationMap.get(0).get("location_id"));
+
+        System.out.println("============== GET FIRST LOCATION FIRST LINK ============== " );
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
